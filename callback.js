355f7d731bd133c8ce5d8e3595076c3634118850
atikdev-bd <atikdev.bd@gmail.com>
2022-09-10 19:32:47 +0600

    practice common js

---
+++ callback.js
@@ -0,0 +1,23 @@
+// function getting(callback, name, tokhim) {
+//   callback(name,tokhim);
+// }
+
+// function callback(name,tokhim) {
+//   console.log(" HI ! Good morning", name,tokhim);
+// }
+// function gettingEvining (name,tokhim){
+// console.log('HI ! Good evening',name,tokhim)
+
+// }
+// const name = "Mr Been";
+// const tokhim = 'welcome to the party'
+// // getting(callback,'Mr Been',tokhim);
+// // getting(callback,'Mr Mizan',tokhim);
+// // getting(callback,'Mr Piash',tokhim);
+// getting(gettingEvining ,name,tokhim);
+const isTrue='false'; 
+if(!isTrue){
+console.log('hello');
+} else {
+console.log('world'); 
+}
